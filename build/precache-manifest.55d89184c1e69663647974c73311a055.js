2d2309bf58881ab6031000d55d13be437cd4d0f4
ginNyang2 <bbsnake10@gmail.com>
2018-11-22 17:11:47 +0900

    first commit

---
+++ build/precache-manifest.55d89184c1e69663647974c73311a055.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e2682c0f5858bea09b99",
+    "url": "/static/js/main.e2682c0f.chunk.js"
+  },
+  {
+    "revision": "680045a5659733b4d7ac",
+    "url": "/static/js/1.680045a5.chunk.js"
+  },
+  {
+    "revision": "e2682c0f5858bea09b99",
+    "url": "/static/css/main.91521117.chunk.css"
+  },
+  {
+    "revision": "47d6e3bc621fc90aea068ae32e179d0f",
+    "url": "/index.html"
+  }
+];
